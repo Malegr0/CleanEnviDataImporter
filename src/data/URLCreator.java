aff004862579246494c11286bfb635e75b8f1394
Malegr0 <schoening.andreas@gmx.de>
2021-01-30 14:18:02 +0100

    added class URLCreator

---
+++ src/data/URLCreator.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class URLCreator {
+
+    public static void sendGetRequest() throws IOException {
+
+    }
+
+}
